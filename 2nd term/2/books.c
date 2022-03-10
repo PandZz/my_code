2e1807095ac2940b47f134b7932c8e902e2d8522
PandZz <2739468442@qq.com>
2022-03-11 00:53:12 +0800

    [Feature] add:more function

--- 2nd term/2/books.c
+++ 2nd term/2/books.c
@@ -12,13 +12,21 @@ typedef struct books
     char name[55], author[25], press[35], date[15];
 } book;
 book lib[600];
-book *index[600];
-int n = 0;
+book *indexL[600];
+int n = 0, nLib;
 
 void convert(book *, char *);
 int cmp(const void *, const void *);
 void print(const book *);
-int lower_bound(book *);
+void fprint(FILE *, const book *);
+//在指标序号l,r之间寻找第一个>=book *b的指标
+int lower_bound(book *, int, int);
+//在int pre后面插入book *b
+void insert(int, book *);
+//从int x位置开始找含有char *s的指标，没有返回-1
+int find(int, char *);
+//删除int x位置的指标
+void del(int);
 
 int main()
 {
@@ -27,14 +35,49 @@ int main()
     fw = fopen("ordered.txt", "w");
     if (fr == NULL || fw == NULL)
         return -1;
-    char tmp[200];
-    while (fgets(tmp, 200, fr) != NULL)
+    char tmpS[200];
+    while (fgets(tmpS, 200, fr) != NULL)
     {
-        index[n] = lib + n;
-        convert(index[n], tmp);
+        indexL[n] = lib + n;
+        convert(indexL[n], tmpS);
         ++n;
     }
-    qsort(index, n, sizeof(book *), cmp);
+    nLib = n;
+    qsort(indexL, n, sizeof(book *), cmp);
+    int op, x;
+    book tmp;
+    while (~scanf("%d", &op) && op)
+    {
+        gets(tmpS);
+        switch (op)
+        {
+        case 1:
+            convert(&tmp, tmpS);
+            lib[nLib++] = tmp;
+            insert(lower_bound(&tmp, 0, n - 1) - 1, &lib[nLib - 1]);
+            break;
+        case 2:
+            x = 0;
+            while (~x)
+            {
+                x = find(x, tmpS);
+                if (~x)
+                    print(indexL[x]);
+            }
+            break;
+        case 3:
+            x = 0;
+            while (~x)
+            {
+                x = find(x, tmpS);
+                if (~x)
+                    del(x);
+            }
+            break;
+        }
+    }
+    for (int i = 0; i < n; ++i)
+        fprint(fw, indexL[i]);
     fclose(fr);
     fclose(fw);
     return 0;
@@ -68,16 +111,48 @@ void print(const book *b)
     printf("%-10s\n", b->date);
 }
 
-int lower_bound(book *b)
+void fprint(FILE *fp, const book *b)
 {
-    int l = 0, r = n - 1, mid;
+    fprintf(fp, "%-50s ", b->name);
+    fprintf(fp, "%-20s ", b->author);
+    fprintf(fp, "%-30s ", b->press);
+    fprintf(fp, "%-10s\n", b->date);
+}
+
+//可能找不到，得改
+int lower_bound(book *b, int l, int r)
+{
+    int mid;
     while (l < r)
     {
         mid = (l + r) / 2;
-        if (strcmp(index[mid]->name, b->name) < 0)
+        if (strcmp(indexL[mid]->name, b->name) < 0)
             l = mid + 1;
         else
             r = mid;
     }
     return l;
+}
+
+void insert(int pre, book *b)
+{
+    for (int i = n - 1; i > pre; --i)
+        indexL[i + 1] = indexL[i];
+    indexL[pre + 1] = b;
+    ++n;
+}
+
+int find(int i, char *s)
+{
+    for (; i < n; ++i)
+        if (strstr(indexL[i]->name, s) != NULL)
+            return i;
+    return -1;
+}
+
+void del(int x)
+{
+    for (int i = x + 1; i < n; ++i)
+        indexL[i - 1] = indexL[i];
+    --n;
 }
